b3cf7798bc81da764c63967af0938d142cf39c9a
ujwalnayak3 <115396876+ujwalnayak3@users.noreply.github.com>
2026-04-11 15:41:29 +0530

    Update info.py

--- info.py
+++ info.py
@@ -136,7 +136,7 @@ START_IMG = (
 FORCESUB_IMG = environ.get("FORCESUB_IMG", "https://i.ibb.co/ZNC1Hnb/ad3f2c88a8f2.jpg")
 REFER_PICS = (environ.get("REFER_PICS", "https://envs.sh/PSI.jpg")).split()
 PAYPICS = (
-    environ.get("PAYPICS", "https://graph.org/file/f4db1c3ad3d9e38b328e6.jpg")
+    environ.get("PAYPICS", "https://graph.org/file/71f2a9a8a6219aa0cc670-0d6aca3754e7a37372.jpg")
 ).split()
 SUBSCRIPTION = environ.get(
     "SUBSCRIPTION", "https://graph.org/file/9f3f47c690bbcc67633c2.jpg"
