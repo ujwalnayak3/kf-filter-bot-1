0272ef22e62c3ca029ad67a769b1ddf15aee334d
ujwalnayak3 <115396876+ujwalnayak3@users.noreply.github.com>
2026-04-12 09:47:33 +0530

    Update info.py

--- info.py
+++ info.py
@@ -49,7 +49,7 @@ DATABASE_URI = environ.get("DATABASE_URI", "")
 DATABASE_NAME = environ.get("DATABASE_NAME", "Cluster0")
 
 # Files index database url
-FILES_DATABASE = environ.get("FILES_DATABASE", " mongodb+srv://ujwal:nayakujwal1@cluster0.olxb2bz.mongodb.net/?appName=Cluster0")
+FILES_DATABASE = environ.get("FILES_DATABASE", "mongodb+srv://ujwal:nayakujwal1@cluster0.olxb2bz.mongodb.net/?appName=Cluster0")
 COLLECTION_NAME = environ.get("COLLECTION_NAME", "jisshu")
 
 # Other Channel's
