22e611070631bf393e19c78801ee2bea25a5a226
ujwalnayak3 <115396876+ujwalnayak3@users.noreply.github.com>
2026-07-09 17:21:02 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -117,7 +117,7 @@ User - {}"""
 ⭐ Rating: {rating} / 10</b>
 """
 
-    FILE_CAPTION = """<b>📂 Join - @Kannada_Cineflix  </b>"""
+    FILE_CAPTION = """<b>{file_name}</b>"""
 
     ALRT_TXT = """ᴡʜᴀᴛ ᴀʀᴇ ʏᴏᴜ sᴇᴀʀᴄʜɪɴɢ !?"""
 
